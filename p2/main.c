c06facb98fcf93a8f29e2ee18c1524e94d94fe12
2025138029 <seungjukim1124@gmail.com>
2026-04-15 16:31:28 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int limit = 0, ncnt = 0, wcnt = 0, ncnt_max = 0, wcnt_max = 0, i = 0;
+    char sen;
+    scanf("%d", &limit);
+    
+    for(i = 0; i < limit; i ++){
+        scanf("%c", &sen);
+        
+        if(sen >= 'a' && sen <= 'z'){
+            wcnt ++;
+            if(wcnt > wcnt_max)
+                wcnt_max = wcnt;
+            ncnt = 0;
+            
+        }
+        
+        if(sen >= '0' && sen <= '9'){
+            ncnt ++;
+            if(ncnt > ncnt_max)
+                ncnt_max = ncnt;
+            wcnt = 0;
+            
+        }
+    }
+    printf("%d\n%d", wcnt_max, ncnt_max);    
+    return 0;
+}
