dee2a682693a96ebc35f4d830cbedc5ee6d295f4
2025138029 <seungjukim1124@gmail.com>
2026-04-15 15:30:05 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int answer = 0, index = 0, a = 0;
+    scanf("%d", &answer);
+    
+    do {
+        scanf("%d", &a);
+        if(answer > a)
+            printf("%d<?", a);
+        if(answer < a)
+            printf("%d>?", a);
+        index ++;
+    } while(answer != a);
+    printf("%d==?/n%d", a, index);
+    return 0;
+}
